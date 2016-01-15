fd513c9732a2e79d1b3a7b2c136916fbabbcfe38
Sanqing Yuan <sanqing.yuan@gmail.com>
2016-01-15 01:56:22 -0800

    Update functions.php

--- functions/functions.php
+++ functions/functions.php
@@ -1,48 +1,43 @@
 <?
-	session_start();
+    session_start();
 
-	function getQuote($symbol) {
-		// try to get quote
-		$handle = @fopen("http://download.finance.yahoo.com/d/quotes.csv?s={$symbol}&f=sl1d1t1c1ohgv", "r");
-
-		if ($handle !== FALSE)
-		{
-			$data = fgetcsv($handle);
-			if ($data !== FALSE) {
-				return $data[1];
-			}
-		}		
-	}
+    function getQuote($symbol) {
+        // try to get quote
+	$handle = @fopen("http://download.finance.yahoo.com/d/quotes.csv?s={$symbol}&f=sl1d1t1c1ohgv", "r");
+	if ($handle !== FALSE) {
+	    $data = fgetcsv($handle);
+	    if ($data !== FALSE) {
+	        return $data[1];
+	    }
+	}		
+    }
 	
 	
-	function load_page($template) {
-		$path = '../views/' . $template;
-        if (file_exists($path))
-        {
+    function load_page($template) {
+        $path = '../views/' . $template;
+        if (file_exists($path)) {
             require($path);
         }
-		else {
-			echo "path not found!";
-		}
+	else {
+	    echo "path not found!";
 	}
+    }
 	
 	
-	function connectDB($myDB) {
-		$servername = "localhost";
-		$username = "root";
-		$password = "19810925";
-		//$myDB = "stock";
-		
-		try {
-			//connect to server and the chosen database
-			$conn = new PDO("mysql:host=$servername;dbname=$myDB", $username, $password);
-			// set the PDO error mode to exception
-			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);		
-			//echo "Connected successfully"; 
-		}
-		catch(PDOException $e) {
-			echo "Connection failed: " . $e->getMessage();
-		}
-		return $conn;
+    function connectDB($myDB) {
+        $servername = "localhost";
+        $username = "root";
+        $password = "19810925";
+        try {
+            //connect to server and the chosen database
+            $conn = new PDO("mysql:host=$servername;dbname=$myDB", $username, $password);
+	    // set the PDO error mode to exception
+	    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);		
+	    //echo "Connected successfully"; 
+        }
+	catch(PDOException $e) {
+	    echo "Connection failed: " . $e->getMessage();
 	}
+	return $conn;
+    }
 ?>
