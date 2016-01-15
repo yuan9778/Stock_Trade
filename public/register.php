2f74b59f962a04527b9bb5516034b8af62277df3
Sanqing Yuan <sanqing.yuan@gmail.com>
2016-01-15 02:02:39 -0800

    Update register.php

--- public/register.php
+++ public/register.php
@@ -1,10 +1,9 @@
 <?
-	
-	// load functions
+    // load functions
     require('../functions/functions.php');
 
-	load_page("banner.html");
-	load_page("log_reg.php");
-	load_page("registrationForm.php");
-	load_page("footer.html");
+    load_page("banner.html");
+    load_page("log_reg.php");
+    load_page("registrationForm.php");
+    load_page("footer.html");
 ?>
