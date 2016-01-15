e9d0e3175276eb1740f5143f3002d9ddca1e8219
Sanqing Yuan <sanqing.yuan@gmail.com>
2016-01-15 01:56:58 -0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,15 +1,14 @@
 <?	
-	// load functions
+    // load functions
     require('../functions/functions.php');
    
-	$_SESSION['exist'] = false;
-	$_SESSION['match'];
-	
-	
-	load_page('banner.html');
-	load_page("log_reg.php");
-	load_page("quote.html"); 
-	load_page("footer.html");
+    $_SESSION['exist'] = false;
+    $_SESSION['match'];
+
+    load_page('banner.html');
+    load_page("log_reg.php");
+    load_page("quote.html"); 
+    load_page("footer.html");
 ?>
 
 
